977c469025f21c2212478a52b2d35665cd55c906
jaime.hernandez <jaime.hernandez@bi-geek.com>
2019-12-27 11:43:14 +0100

    Initial commit

---
+++ src/main/java/com/jho/halterocmsjpa/HalterocmsjpaApplication.java
@@ -0,0 +1,13 @@
+package com.jho.halterocmsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalterocmsjpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HalterocmsjpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jho/halterocmsjpa/HalterocmsjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jho.halterocmsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalterocmsjpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
